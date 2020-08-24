db78c21e1134ee9461d518051517f730fa0e8df4
雷振宇 <aleigege008@qq.com>
2020-08-25 03:44:05 +0800

    test

--- springcloud-web-admin-feign/src/main/java/com/lzy/common/springcloud/web/admin/fegn/WebAdminFeignApplication.java
+++ springcloud-web-admin-feign/src/main/java/com/lzy/common/springcloud/web/admin/fegn/WebAdminFeignApplication.java
@@ -3,11 +3,14 @@ package com.lzy.common.springcloud.web.admin.fegn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrixDashboard
 public class WebAdminFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminFeignApplication.class,args);
